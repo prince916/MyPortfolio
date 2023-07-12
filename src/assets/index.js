5a8854c5f2f6354e4def8b4a44a8e8214ed7dce6
Your Name <you@example.com>
2023-07-12 11:07:26 +0530

    new commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,13 @@
 import logo from "./images/testimonial/logo.png";
-import SnakeGame from "./images/projects/SnakeGame.png"
-import Mypic2 from "./images/testimonial/Mypic2.png"
+import SnakeGame from "./images/projects/SnakeGame.png";
+import Mypic2 from "./images/testimonial/Mypic2.png";
 import Myself from "./images/testimonial/Myself.png";
+import ContactMe from "./images/contact/ContactMe.jpg";
 
 export {
     logo,
     SnakeGame,
     Myself,
-    Mypic2
+    Mypic2,
+    ContactMe
 };
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "../layouts/Title";
+import { ContactMe } from "../../assets/index";
 
 const Contact = () => {
   return (
@@ -8,17 +9,28 @@ const Contact = () => {
       className="w-full py-20 border-b-[1px] border-b-black"
     >
       <div className="flex justify-center items-center text-center">
-        <Title
-          title="CONTACT"
-          des="Contact With Me"
-        />
+        <Title title="CONTACT" des="Contact With Me" />
       </div>
       <div className="w-full">
-        <div className="w-full h-[500px] flex flex-col justify-between">
-            <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
-
-            </div>
-            <div></div>
+        <div className="w-full h-[500px] flex justify-between">
+          <div
+            className="w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] 
+            flex flex-col gap-8 p-6 lgl:p-8 rounded-lg shadow-shadowOne"
+          >
+            <img
+              className="w-full h-64 object-cover rounded-lg mb-2"
+              src={ContactMe}
+              alt="ContactMe"
+            />
+          </div>
+          <div className="w-[60%] h-full flex flex-col justify-between rounded-lg bg-blue-200">
+            <form>
+              <label>
+                Enter your name:
+                <input type="text" />
+              </label>
+            </form>
+          </div>
         </div>
       </div>
     </section>
