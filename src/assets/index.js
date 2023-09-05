1da5123303acca2d4143eb29ebd9b512eb95dfb4
Your Name <you@example.com>
2023-09-06 01:05:14 +0530

    updatethis

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,7 @@
 import logo from "./images/testimonial/logo.png";
 import SnakeGame from "./images/projects/SnakeGame.png";
 import Mypic2 from "./images/testimonial/Mypic2.png";
+import Mypic3 from "./images/testimonial/Mypic3.png";
 import Myself from "./images/testimonial/Myself.png";
 import ContactMe from "./images/contact/ContactMe.jpg";
 import project2 from "./images/projects/project2.png";
@@ -11,6 +12,7 @@ export {
     SnakeGame,
     Myself,
     Mypic2,
+    Mypic3,
     ContactMe,
     project2,
     project3
