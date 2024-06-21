1fc9bf2cf9d09a4504e3a4e0beba51a5d96fc701
Your Name <kumarprince08081999@gmail.com>
2024-06-21 22:33:36 +0530

    newcommit

--- src/components/resume/Skills.jsx
+++ src/components/resume/Skills.jsx
@@ -82,7 +82,7 @@ const Skills = () => {
         </div>
         <div className="flex flex-col gap-6">
           <div className="overflow-x-hidden">
-            <p className="text-sm uppercase font-medium">React</p>
+            <p className="text-sm uppercase font-medium">REACTt</p>
             <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
               <motion.span
                 initial={{ x: "-100%", opacity: 0 }}
